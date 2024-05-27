5f79e69a5a6741c6151f238b1cd876fa07cc68ef
AlexandrIvanov <alexandr.1996@list.ru>
2024-05-27 17:53:52 +0300

    init: add dependencies and  pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang={'en'}>
       <Head />
+      <header
+        className={
+          'flex items-center justify-space gap-5 w-full h-10 border-solid border-b-2 border-black'
+        }
+      >
+        <Link href={'/'}>Main</Link>
+        <Link href={'/sign-in'}>Sign in</Link>
+        <Link href={'/sign-up'}>Sign up</Link>
+        <Link href={'/profile'}>Profile</Link>
+      </header>
       <body>
         <Main />
         <NextScript />
