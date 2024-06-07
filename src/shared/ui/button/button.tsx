ccd947b0d84c2f7ab2b7a3169de3836f668ab26e
Alexandr Kuchutkov <134509128+mrAxekash@users.noreply.github.com>
2024-06-07 21:32:39 +0300

    Button component (#4)

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,29 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import clsx from 'clsx'
+
+import s from './button.module.scss'
+
+export type ButtonVariant = 'link' | 'outlined' | 'primary' | 'secondary' | 'text'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  children: ReactNode
+  className?: string
+  fullWidth?: boolean
+  variant?: ButtonVariant
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const { as, className, fullWidth, variant = 'primary', ...rest } = props
+
+  const classNames = { root: clsx(fullWidth && s.fullWidth, s[variant], className) }
+
+  const Component = as || 'button'
+
+  return (
+    <Component className={classNames.root} {...rest}>
+      {props.children}
+    </Component>
+  )
+}
