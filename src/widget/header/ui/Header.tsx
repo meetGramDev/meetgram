be505c77798a1ec289d77ddd404154ee90c179c4
Golysheva Laura <forx98@gmail.com>
2024-06-09 00:28:41 +0300

    feat: make header component
    * style header
    
    * make storybook
    
    * style: message counter on notification icon
    
    * fix styles
    
    * chore: add layout for header storybook
    
    * fix: make notification icon into button
    
    * chore: add index.ts

---
+++ src/widget/header/ui/Header.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { Notification } from '@/shared/assets/icons/Notification'
+import { Button } from '@/shared/ui/button/button'
+import { OptionType } from '@/shared/ui/select/option'
+import { Select } from '@/shared/ui/select/select'
+import Link from 'next/link'
+
+const languages: OptionType[] = [
+  { label: 'Russian', value: 'ru' },
+  { label: 'English', value: 'en' },
+]
+
+type Props = {
+  isAuth?: boolean
+  notification?: number
+}
+
+export const Header = ({ isAuth = true, notification }: Props) => {
+  return (
+    <header
+      className={
+        'flex h-[3.75rem] w-full items-center justify-between border-b border-[#333] px-[3.75rem]'
+      }
+    >
+      <Link className={'text-large font-semibold'} href={'/'}>
+        Meetgram
+      </Link>
+      <div className={'flex items-center justify-between gap-[2.25rem]'}>
+        <div className={'flex items-center justify-end gap-[1.5rem]'}>
+          {isAuth && (
+            <Button variant={'text'}>
+              <div className={'relative text-light-100'}>
+                <Notification
+                  className={'fill-current transition-all duration-300 hover:fill-accent-500'}
+                />
+                {notification && (
+                  <div
+                    className={
+                      'absolute left-[10px] top-[-5px] flex aspect-square h-[13px] items-center justify-center rounded-full bg-danger-500 px-1 text-[0.625rem] text-light-100'
+                    }
+                  >
+                    {notification}
+                  </div>
+                )}
+              </div>
+            </Button>
+          )}
+          <Select defaultValue={'ru'} options={languages} />
+        </div>
+
+        {!isAuth && (
+          <div className={'flex gap-6'}>
+            <Button variant={'text'}>Log in</Button>
+            <Button variant={'primary'}>Sign up</Button>
+          </div>
+        )}
+      </div>
+    </header>
+  )
+}
