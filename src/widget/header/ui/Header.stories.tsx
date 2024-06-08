aabfc64648edca32c27caaf527c350b59b22db66
m1rade <forx98@gmail.com>
2024-06-08 04:58:12 +0300

    make storybook

---
+++ src/widget/header/ui/Header.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Header } from '@/widget/header/ui/Header'
+
+const meta = {
+  component: Header,
+  tags: ['autodocs'],
+  title: 'Widget/Header',
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    notification: 3,
+  },
+}
+
+export const NotAuthorized: Story = {
+  args: {
+    isAuth: false,
+  },
+}
