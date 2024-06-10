4d0457fd530c56d45c521e488ad383f7cc1ff8a2
mrAxekash <aliaxandr_ku@mail.ru>
2024-06-10 23:11:50 +0300

    feat: add module styles forgotPassword, add stories forgotPassword

---
+++ src/featuers/auth/forgotPassword/ui/ForgotPasswordForm.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/shared/ui/button/button'
+import Card from '@/shared/ui/card/Card'
+import { Input } from '@/shared/ui/input/input'
+
+export const ForgotPasswordForm = () => {
+  return (
+    <Card>
+      <h1>Forgot Password</h1>
+      <form>
+        <Input label={'Email'} placeholder={'Google@gmail.com'} />
+        <div>Enter your email and we will send you further instruction</div>
+        <Button fullWidth type={'submit'} variant={'primary'}>
+          Send Link
+        </Button>
+      </form>
+      <Button as={'link'} href={'#'} variant={'link'}>
+        Back to Sign In
+      </Button>
+    </Card>
+  )
+}
--- src/pages/password-recovery/RecoveryPassword.tsx
+++
@@ -1,9 +0,0 @@
-import Card from '@/shared/ui/card/Card'
-
-export const RecoveryPassword = () => {
-  return (
-    <Card>
-      <h1>Forgot Password</h1>
-    </Card>
-  )
-}
