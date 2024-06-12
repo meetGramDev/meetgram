92100e0235a17110d5f2a802286cfe109272218d
mrAxekash <aliaxandr_ku@mail.ru>
2024-06-13 00:14:07 +0300

    feat: add styles in form p.1

--- src/featuers/auth/forgotPassword/ui/ForgotPasswordForm.tsx
+++ src/featuers/auth/forgotPassword/ui/ForgotPasswordForm.tsx
@@ -1,19 +1,33 @@
+import { cn } from '@/shared/lib/cn'
 import { Button } from '@/shared/ui/button/button'
-import Card from '@/shared/ui/card/Card'
+import { Card } from '@/shared/ui/card'
 import { Input } from '@/shared/ui/input/input'
+import Link from 'next/link'
+
+import s from './forgotPassword.module.scss'
 
 export const ForgotPasswordForm = () => {
+  const classNames = {
+    buttonC: cn('mb-6'),
+    link: cn(''),
+    subtitle: cn('mb-4 text-sm text-light-900'),
+    textField: cn('mb-2'),
+    title: cn('text-lg font-bold leading-9 mb-9'),
+  }
+
   return (
-    <Card>
-      <h1>Forgot Password</h1>
+    <Card className={s.card}>
+      <h1 className={classNames.title}>Forgot Password</h1>
       <form>
-        <Input label={'Email'} placeholder={'Google@gmail.com'} />
-        <div>Enter your email and we will send you further instruction</div>
-        <Button fullWidth type={'submit'} variant={'primary'}>
+        <Input className={classNames.textField} label={'Email'} placeholder={'Google@gmail.com'} />
+        <div className={classNames.subtitle}>
+          Enter your email and we will send you further instruction
+        </div>
+        <Button className={classNames.buttonC} fullWidth type={'submit'} variant={'primary'}>
           Send Link
         </Button>
       </form>
-      <Button as={'Link'} href={'#'} variant={'link'}>
+      <Button as={Link} className={classNames.link} href={'#'} variant={'link'}>
         Back to Sign In
       </Button>
     </Card>
