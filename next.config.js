0eda12de966560d97d5a3d1badb15fc7602138d5
VladimirKivg <cherenuha1@gmail.com>
2024-06-12 22:41:52 -0700

    added i18n into dependencies

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   env: {
     captchaSiteKey: '6Le9h_IpAAAAAF6U0_jL6SNQKTXC_IuBTp-5ksOr',
   },
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['en', 'ru'],
+  },
   reactStrictMode: true,
 }
 
