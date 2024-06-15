6354197f4df9792b74fb171f6f4c031f386c2e3f
AlexandrIvanov <alexandr.1996@list.ru>
2024-06-15 03:15:01 +0300

    Add headers

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,8 +3,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { BASE_URL } from './baseUrl'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
-  endpoints: builder => ({}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: headers => {
+      headers.set('Access-Control-Allow-Origin', '*')
+
+      return headers
+    },
+  }),
+  endpoints: () => ({}),
   reducerPath: 'meetGramApi',
   tagTypes: ['login', 'auth'],
 })
