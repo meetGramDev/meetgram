2f7e0fd3e93b726d1af4be2b1343d8bf00df50e0
mrAxekash <aliaxandr_ku@mail.ru>
2024-06-17 00:33:17 +0300

    feat: add styles in form p.2

--- src/featuers/auth/forgotPassword/ui/ForgotPasswordForm.tsx
+++ src/featuers/auth/forgotPassword/ui/ForgotPasswordForm.tsx
@@ -1,35 +1,59 @@
-import { cn } from '@/shared/lib/cn'
+import ReCAPTCHA from 'react-google-recaptcha'
+
 import { Button } from '@/shared/ui/button/button'
 import { Card } from '@/shared/ui/card'
 import { Input } from '@/shared/ui/input/input'
+import { clsx } from 'clsx'
 import Link from 'next/link'
 
 import s from './forgotPassword.module.scss'
 
-export const ForgotPasswordForm = () => {
+import { ForgotPasswordFormData, useForgotPassword } from './useForgotPassword'
+
+type ForgotPasswordType = {
+  onSubmit: (data: ForgotPasswordFormData) => void
+}
+
+export const ForgotPasswordForm = ({ onSubmit }: ForgotPasswordType) => {
   const classNames = {
-    buttonC: cn('mb-6'),
-    link: cn(''),
-    subtitle: cn('mb-4 text-sm text-light-900'),
-    textField: cn('mb-2'),
-    title: cn('text-lg font-bold leading-9 mb-9'),
+    btnLink: clsx(s.bntLink),
+    button: clsx(s.button),
+    card: clsx(s.card),
+    input: clsx(s.input),
+    text: clsx(s.text),
+    title: clsx(s.title),
+  }
+
+  const { errors, handleSubmit, register } = useForgotPassword()
+
+  const siteKey = '6Le9h_IpAAAAAF6U0_jL6SNQKTXC_IuBTp-5ksOr'
+
+  const onChange = (value: any) => {
+    console.log('CaptchaValue', value)
   }
 
   return (
-    <Card className={s.card}>
+    <Card className={classNames.card}>
       <h1 className={classNames.title}>Forgot Password</h1>
-      <form>
-        <Input className={classNames.textField} label={'Email'} placeholder={'Google@gmail.com'} />
-        <div className={classNames.subtitle}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          className={classNames.input}
+          label={'Email'}
+          {...register('email')}
+          error={errors.email?.message}
+          placeholder={'Google@gmail.com'}
+        />
+        <div className={classNames.text}>
           Enter your email and we will send you further instruction
         </div>
-        <Button className={classNames.buttonC} fullWidth type={'submit'} variant={'primary'}>
+        <Button className={classNames.button} fullWidth type={'submit'} variant={'primary'}>
           Send Link
         </Button>
+        <Button as={Link} className={classNames.btnLink} href={'#'} variant={'link'}>
+          Back to Sign In
+        </Button>
+        <ReCAPTCHA lang={'en'} onSubmit={onChange} sitekey={siteKey} theme={'dark'} />
       </form>
-      <Button as={Link} className={classNames.link} href={'#'} variant={'link'}>
-        Back to Sign In
-      </Button>
     </Card>
   )
 }
