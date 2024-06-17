13cc725d034af915b45594d59f2d8a814398edf2
Golysheva Laura <forx98@gmail.com>
2024-06-17 19:55:15 +0300

    Feat/sign in logic (#19) fix bug in login api slice

--- src/entities/user/api/loginApiSlice.ts
+++ src/entities/user/api/loginApiSlice.ts
@@ -1,16 +1,17 @@
 import { BASE_URL } from '@/shared/api'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { ArgsLogin } from './loginApiSlice.types'
+import { ArgsLogin, SignInSuccessResponse } from './loginApiSlice.types'
+
 
 export const loginApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: builder => ({
-    login: builder.mutation<any, ArgsLogin>({
+    login: builder.mutation<SignInSuccessResponse, ArgsLogin>({
       query: args => ({
-        body: { args },
         method: 'POST',
         url: '/auth/login',
+        body: { ...args },
       }),
     }),
   }),
