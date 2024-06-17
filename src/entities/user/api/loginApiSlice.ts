48ea3df794e5274d203e887519756a42e7ffd946
m1rade <forx98@gmail.com>
2024-06-17 19:41:50 +0300

    fix: bug in login api slice

--- src/entities/user/api/loginApiSlice.ts
+++ src/entities/user/api/loginApiSlice.ts
@@ -1,14 +1,14 @@
 import { BASE_URL } from '@/shared/api'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { ArgsLogin } from './loginApiSlice.types'
+import { ArgsLogin, SignInSuccessResponse } from './loginApiSlice.types'
 
 export const loginApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: builder => ({
-    login: builder.mutation<any, ArgsLogin>({
+    login: builder.mutation<SignInSuccessResponse, ArgsLogin>({
       query: args => ({
-        body: { args },
+        body: { ...args },
         method: 'POST',
         url: '/auth/login',
       }),
