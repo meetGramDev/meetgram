8ecd82c32825f9fd88c6112a3bade331fe1cd975
AlexandrIvanov <alexandr.1996@list.ru>
2024-06-26 15:49:41 +0300

    Refactor: fix onSubmit function, add server errors

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,21 +1,65 @@
-import { SignUpForm, SignUpFormData, useSignUpMutation } from '@/features/auth/signUp'
-import { NextPageWithLayout } from '@/shared/types'
+import { useState } from 'react'
+
+import {
+  SignUpForm,
+  SignUpFormData,
+  authSliceActions,
+  getEmail,
+  useSignUpMutation,
+} from '@/features/auth/signUp'
+import { Tr } from '@/hooks/useLangSwitcher'
+import { ServerBadResponse } from '@/shared/api'
+import { useAppDispatch, useAppSelector } from '@/shared/config/storeHooks'
+import { NextPageWithLayout, isFetchBaseQueryError } from '@/shared/types'
+import { Button } from '@/shared/ui'
+import { Dialog } from '@/shared/ui/dialog'
 import { getAuthLayout } from '@/widgets/layouts'
+import { useRouter } from 'next/router'
+
+import s from './index.module.scss'
 
 const SignUp: NextPageWithLayout = () => {
-  // const [signUp] = useSignUpMutation()
+  const [signUp] = useSignUpMutation()
+  const [error, setError] = useState([])
+  const email = useAppSelector(getEmail)
+  const dispatch = useAppDispatch()
+  const [open, setOpen] = useState(false)
+  const locale = useRouter().locale
+  const { signUpLang } = Tr(locale)
 
   const onSubmit = async ({ confirmPassword, isApproved, ...data }: SignUpFormData) => {
-    // signUp({ ...data })
-    //   .unwrap()
-    //   .then(() => {
-    //     alert(`We have sent a link to confirm your email to ${data.email}`)
-    //   })
+    try {
+      await signUp({ ...data }).unwrap()
+      dispatch(authSliceActions.setEmail(data.email))
+      setOpen(true)
+    } catch (error) {
+      if (isFetchBaseQueryError(error)) {
+        const errMsg =
+          'error' in error
+            ? error.error
+            : JSON.stringify((error.data as ServerBadResponse).messages)
+
+        setError(JSON.parse(errMsg))
+      }
+    }
   }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
-      <SignUpForm onSubmit={onSubmit} />
+      <SignUpForm error={error} onSubmit={onSubmit} />
+      <>
+        <Dialog onOpenChange={setOpen} open={open} title={'Email sent'}>
+          <div className={s.modalContent}>
+            <div>
+              {signUpLang.aler}
+              {email}
+            </div>
+            <Button onClick={() => setOpen(false)} style={{ alignSelf: 'flex-end' }}>
+              Ok
+            </Button>
+          </div>
+        </Dialog>
+      </>
     </div>
   )
 }
