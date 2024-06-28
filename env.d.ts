56d4c2d7113730bf8604fb90a0e5827cd6f4d6ef
mrAxekash <aliaxandr_ku@mail.ru>
2024-06-29 00:40:33 +0300

    feat: add new branch

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GITHUB_CLIENT_ID: string
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string
+    NEXT_PUBLIC_OAUTH2_REDIRECT_URL: string
+  }
+}
