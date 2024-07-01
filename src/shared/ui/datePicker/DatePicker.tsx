2d4c38320c5b1c59d6945418df89491dd1c87529
m1rade <forx98@gmail.com>
2024-07-01 17:16:08 +0300

    add custom header for calendar popper, locales

--- src/shared/ui/datePicker/DatePicker.tsx
+++ src/shared/ui/datePicker/DatePicker.tsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import ReactDatePicker, { registerLocale } from 'react-datepicker'
 
 import { Calendar } from '@/shared/assets/icons/Calendar'
+import { range } from '@/shared/lib/range'
 import { Nullable } from '@/shared/types'
-import { Input } from '@/shared/ui'
+import { Button, Input, Option, Select } from '@/shared/ui'
 import clsx from 'clsx'
-// import { ru } from 'date-fns/locale/ru'
+import { getMonth } from 'date-fns/getMonth'
+import { getYear } from 'date-fns/getYear'
+import { enUS } from 'date-fns/locale/en-US'
+import { ru } from 'date-fns/locale/ru'
 
 import 'react-datepicker/dist/react-datepicker.css'
 
 import s from './DatePicker.module.scss'
 
-// registerLocale('ru', ru)
-// setDefaultLocale('ru')
+registerLocale('ru-RU', ru)
+registerLocale('en-US', enUS)
 
 type Props = {
   disabled?: boolean
@@ -22,11 +26,46 @@ type Props = {
   selectRange?: boolean
 }
 
+const years = range(1940, getYear(new Date()) + 1, 1)
+const months = {
+  en: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  ru: [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ],
+}
+
+type Locales = keyof typeof months
+
 export const DatePicker = ({ disabled, error, label, required, selectRange }: Props) => {
   const [startDate, setStartDate] = useState<Date | undefined>(new Date())
   const [endDate, setEndDate] = useState<Date | undefined>(undefined)
 
   const locale = navigator.language
+  const localeMonths: Locales = locale.substring(0, 2) as Locales
 
   const classes = {
     hasError: clsx(error && s.hasError),
@@ -69,6 +108,72 @@ export const DatePicker = ({ disabled, error, label, required, selectRange }: Pr
         locale={locale}
         onChange={onRangeChange}
         popperClassName={s.popper}
+        renderCustomHeader={({
+          changeMonth,
+          changeYear,
+          date,
+          decreaseMonth,
+          increaseMonth,
+          nextMonthButtonDisabled,
+          prevMonthButtonDisabled,
+        }) => {
+          return (
+            <div className={s.customHeaderContainer}>
+              <div>
+                <Select
+                  contentClassName={s.monthSelector}
+                  defaultValue={months[localeMonths][getMonth(date)]}
+                  onValueChange={value => changeMonth(+value)}
+                  rootClassName={s.datePickerSelector}
+                  showArrow={false}
+                  value={getMonth(date).toString()}
+                  withPortal={false}
+                >
+                  {months[localeMonths].map((m, i) => (
+                    <Option key={i} value={i.toString()}>
+                      {m}
+                    </Option>
+                  ))}
+                </Select>
+
+                <Select
+                  contentClassName={s.yearSelector}
+                  defaultValue={getYear(date).toString()}
+                  onValueChange={value => changeYear(+value)}
+                  rootClassName={s.datePickerSelector}
+                  showArrow={false}
+                  value={getYear(date).toString()}
+                  withPortal={false}
+                >
+                  {years.map(y => (
+                    <Option key={y} value={y.toString()}>
+                      {y}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+
+              <div>
+                <Button
+                  className={s.changeMonthsBtn}
+                  disabled={prevMonthButtonDisabled}
+                  onClick={decreaseMonth}
+                  variant={'text'}
+                >
+                  {'<'}
+                </Button>
+                <Button
+                  className={s.changeMonthsBtn}
+                  disabled={nextMonthButtonDisabled}
+                  onClick={increaseMonth}
+                  variant={'text'}
+                >
+                  {'>'}
+                </Button>
+              </div>
+            </div>
+          )
+        }}
         selected={startDate}
         selectsRange={selectRange || undefined}
         shouldCloseOnSelect={!selectRange}
