8ab635f2ba46eea00da2b083455bbe41408d32ed
m1rade <forx98@gmail.com>
2024-07-06 13:28:40 +0300

    fix: predicates funcs export error

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,4 @@
 export type { Nullable } from './Nullable.type'
 export type { DefinePropertyType } from './definePropertyType'
-export type * from './errorsTypePredicates'
+export * from './errorsTypePredicates'
 export type { NextPageWithLayout } from './pageWithLayout.type'
