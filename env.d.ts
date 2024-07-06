8c15121a0d9109ba26c37e7c6712e52f9d4fdcea
Alexandr Kuchutkov <134509128+mrAxekash@users.noreply.github.com>
2024-07-06 22:33:31 +0300

    Delete env.d.ts

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: string
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string
-    NEXT_PUBLIC_OAUTH2_REDIRECT_URL: string
-  }
-}
