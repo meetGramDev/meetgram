06478e8dfc428aef6dd9b6a81a555918507ac7b0
AlexandrIvanov <alexandr.1996@list.ru>
2024-07-08 13:36:28 +0300

    Feat/mobile-header

--- src/widgets/layouts/ui/AuthLayout.tsx
+++ src/widgets/layouts/ui/AuthLayout.tsx
@@ -1,14 +1,18 @@
 import type { ReactElement, ReactNode } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 import { Header } from '@/widgets/header'
+import { MobileHeader } from '@/widgets/mobileHeader'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 480px)' })
+
   return (
     <div className={`min-h-[calc(100vh_-_3.75rem_-_72px)] space-y-9 pb-9 ${inter.className}`}>
-      <Header />
+      {isMobile ? <MobileHeader /> : <Header />}
       <main className={'flex h-full items-start justify-center'}>{children}</main>
     </div>
   )
