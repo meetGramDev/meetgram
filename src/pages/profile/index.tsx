c92f5f82d1e1ca44269e81152b413f7d9f59f3fd
VyacheslavSavchik <rothmans.830@gmail.com>
2024-07-10 16:09:59 +0300

    change export profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,9 @@
 import { useMeQuery } from '@/entities/user'
 
-const Profile = () => {
+export default function Profile() {
   const { data } = useMeQuery()
 
   console.log(data)
 
   return <div>Profile</div>
 }
-
-export default Profile
