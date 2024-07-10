c12e2abaf427f974626bd80bc5b825f5d14c2f18
VladimirKivg <cherenuha1@gmail.com>
2024-07-09 23:17:37 -0700

    refactoring

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-import { getSidebarEl } from '@/shared/assets/icons/sidbarIcon/index'
 import { translate } from '@/shared/lib/langSwitcher'
-import { SidebarPng } from '@/widgets/sidebar/ui/sidebar-png/SidebarPng'
+import { getSidebarEl } from '@/widgets/sidebar/ui/index'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -20,27 +19,36 @@ export const Sidebar: React.FC = ({ className, pngSize }: TypeProps) => {
   const { bottom, middle, top } = getSidebarEl(sidebarTr)
 
   return (
-    <aside className={s.border_in}>
-      <div className={clsx(clsx(s.space, s.space_top), [className])}>
+    <aside className={s.border}>
+      <div className={clsx(clsx(s.gap, s.gapTop), [className])}>
         {top.map((el, i) => (
           <Link href={el.path} key={i} passHref>
-            <SidebarPng className={s.link} name={el.name} png={el.png} size={pngSize || 24} />
+            <div className={s.link}>
+              <el.svg />
+              {el.name}
+            </div>
           </Link>
         ))}
       </div>
 
-      <div className={clsx(clsx(s.space, s.space_top_bottom), [className])}>
+      <div className={clsx(clsx(s.gap, s.gapTopAndBottom), [className])}>
         {middle.map((el, i) => (
           <Link href={el.path} key={i} passHref>
-            <SidebarPng className={s.link} name={el.name} png={el.png} size={pngSize || 24} />
+            <div className={clsx(s.link)}>
+              <el.svg />
+              {el.name}
+            </div>
           </Link>
         ))}
       </div>
 
-      <div className={clsx(clsx(s.space, s.space_bottom), [className])}>
+      <div className={clsx(clsx(s.gap, s.gapBottom), [className])}>
         {bottom.map((el, i) => (
           <Link href={el.path} key={i} passHref>
-            <SidebarPng className={s.link} name={el.name} png={el.png} size={pngSize || 24} />
+            <div className={s.link}>
+              <el.svg />
+              {el.name}
+            </div>
           </Link>
         ))}
       </div>
---
+++ src/widgets/sidebar/ui/index.ts
@@ -0,0 +1,41 @@
+import { HOME } from '@/shared/config/router'
+
+import { SidebarTr } from '../../../../public/locales/en'
+import { CreateIcon } from '../../../shared/assets/icons/Create'
+import { FavoritesIcon } from '../../../shared/assets/icons/Favorites'
+import { HomeIcon } from '../../../shared/assets/icons/Home'
+import { LogOutIcon } from '../../../shared/assets/icons/LogOut'
+import { MessengerIcon } from '../../../shared/assets/icons/Messenger'
+import { MyProfileIcon } from '../../../shared/assets/icons/MyProfile'
+import { Search } from '../../../shared/assets/icons/Search'
+import { StatisticsIcon } from '../../../shared/assets/icons/Statistics'
+
+const create = 'Create'
+const favorites = 'Favorites'
+const home = 'Home'
+const logOut = 'Log Out'
+const messenger = 'Messenger'
+const myProfile = 'My Profile'
+const search = 'Search'
+const statistics = 'Statistics'
+
+function getSidebarEl(sidebarTr?: SidebarTr) {
+  return {
+    bottom: [{ name: sidebarTr ? sidebarTr.logOut : logOut, path: '', svg: LogOutIcon }],
+    middle: [
+      { name: sidebarTr ? sidebarTr.favorites : favorites, path: '', svg: FavoritesIcon },
+      { name: sidebarTr ? sidebarTr.statistics : statistics, path: '', svg: StatisticsIcon },
+    ],
+    top: [
+      { name: sidebarTr ? sidebarTr.home : home, path: HOME, svg: HomeIcon },
+      { name: sidebarTr ? sidebarTr.create : create, path: '', svg: CreateIcon },
+      { name: sidebarTr ? sidebarTr.myProfile : myProfile, path: '', svg: MyProfileIcon },
+      { name: sidebarTr ? sidebarTr.messenger : messenger, path: '', svg: MessengerIcon },
+      { name: sidebarTr ? sidebarTr.search : search, path: '', svg: Search },
+    ],
+  }
+}
+
+export type TypePng = typeof HomeIcon
+
+export { getSidebarEl }
