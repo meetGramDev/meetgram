1fbffbd5841a640b0f7e64cb5eb6a0dc9b894482
mrAxekash <aliaxandr_ku@mail.ru>
2024-07-10 23:49:16 +0300

    init: add new block

--- src/entities/user/ui/Profile.tsx
+++ src/entities/user/ui/Profile.tsx
@@ -1,4 +1,4 @@
-import { Photo } from '@/shared/ui'
+import { Button, Photo } from '@/shared/ui'
 import { getAuthLayout } from '@/widgets/layouts'
 import { useSearchParams } from 'next/navigation'
 
@@ -7,10 +7,25 @@ import s from './Profile.module.scss'
 const Profile = () => {
   const params = useSearchParams()
 
+  const name = 'User Name'
+  const following = 2218
+
   return (
     <div className={s.headerWrapper}>
-      <Photo alt={'userPhoto'} src={'/'} />
-      <div></div>
+      {/*<Photo alt={'userPhoto'} src={'/'} />*/}
+      <div>
+        <div>
+          <h1>{name}</h1>
+          <Button variant={'secondary'}>Profile Settings</Button>
+        </div>
+        <div>
+          <Button variant={'link'}>
+            {following}
+            <br />
+            following
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
