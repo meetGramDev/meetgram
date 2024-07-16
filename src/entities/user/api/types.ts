810e61e97925ed5b1738f57b682c7b1c7b4ef32a
m1rade <forx98@gmail.com>
2024-07-16 19:05:42 +0300

    chore: update profile page on uploaded photo

--- src/entities/user/api/types.ts
+++ src/entities/user/api/types.ts
@@ -1,6 +1,20 @@
+import { Avatar } from '@/entities/photo'
+
 export type AuthMeResponseType = {
   email: string
   isBlocked: boolean
   userId: number
+  userName: string
+}
+
+export interface UserProfileResponseType {
+  aboutMe: string
+  avatars: Avatar[]
+  city: string
+  createdAt: string
+  dateOfBirth: string
+  firstName: string
+  id: number
+  lastName: string
   userName: string
 }
--- src/entities/user/api/userApiSlice.ts
+++ src/entities/user/api/userApiSlice.ts
@@ -1,9 +1,14 @@
-import type { AuthMeResponseType } from './types'
+import type { AuthMeResponseType, UserProfileResponseType } from './types'
 
 import { baseApi } from '@/shared/api'
 
 export const userApi = baseApi.injectEndpoints({
   endpoints: builder => ({
+    getUserProfile: builder.query<UserProfileResponseType, void>({
+      query: () => ({
+        url: '/users/profile',
+      }),
+    }),
     me: builder.query<AuthMeResponseType, void>({
       query: () => ({
         url: '/auth/me',
@@ -12,4 +17,13 @@ export const userApi = baseApi.injectEndpoints({
   }),
 })
 
-export const { useLazyMeQuery, useMeQuery } = userApi
+// Export hooks for usage in functional components
+export const {
+  useGetUserProfileQuery,
+  useLazyMeQuery,
+  useMeQuery,
+  util: { getRunningQueriesThunk },
+} = userApi
+
+// export endpoints for use in SSR
+export const { getUserProfile } = userApi.endpoints
