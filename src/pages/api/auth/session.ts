48d6192d00d0360b16b2ea264615028e8aa3ff43
m1rade <forx98@gmail.com>
2024-07-16 21:03:12 +0300

    chore: add get endpoint to session api

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -1,30 +1,41 @@
 import { SESSION_COOKIE_NAME } from '@/shared/const/consts'
-import { serialize } from 'cookie'
+import cookies from 'cookie'
 import { decodeJwt } from 'jose'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<{ message: string }>
-) {
-  if (!req.body) {
-    return res.status(400).json({ message: 'Request must contain a token' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<SessionResponse>) {
+  if (req.method === 'POST') {
+    if (!req.body) {
+      return res.status(400).json({ message: 'Request must contain a token' })
+    }
+    // receive token send from backend
+    const { credentials } = req.body as { credentials: string }
+
+    const { exp } = decodeJwt(credentials)
+
+    res.setHeader(
+      'Set-Cookie',
+      cookies.serialize(SESSION_COOKIE_NAME, credentials, {
+        expires: new Date(exp! * 1000),
+        httpOnly: true,
+        path: '/',
+        sameSite: 'strict',
+        secure: true,
+      })
+    )
+
+    return res.status(200).json({ message: 'Authenticated' })
   }
-  // receive token send from backend
-  const { credentials } = req.body as { credentials: string }
 
-  const { exp } = decodeJwt(credentials)
+  if (req.method === 'GET') {
+    const cookie = cookies.parse(req.headers.cookie || '') as { [SESSION_COOKIE_NAME]: string }
 
-  res.setHeader(
-    'Set-Cookie',
-    serialize(SESSION_COOKIE_NAME, credentials, {
-      expires: new Date(exp! * 1000),
-      httpOnly: true,
-      path: '/',
-      sameSite: 'strict',
-      secure: true,
-    })
-  )
-
-  return res.status(200).json({ message: 'Authenticated' })
+    if (cookie[SESSION_COOKIE_NAME]) {
+      return res.status(200).json({ accessToken: cookie[SESSION_COOKIE_NAME] })
+    } else {
+      return res.status(401).send({ error: 'Unauthenticated' })
+    }
+  }
 }
+
+type SessionResponse = { accessToken: string } | { error: string } | { message: string }
