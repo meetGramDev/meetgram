0588fe75d19d3a2b2800be70c15ca574009a0f6c
m1rade <forx98@gmail.com>
2024-07-19 19:06:38 +0300

    chore: style notification toast component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { NextPageWithLayout } from '@/shared/types'
 import type { AppProps } from 'next/app'
 
-import { ToastContainer } from 'react-toastify'
-
 import { StoreProvider } from '@/app/lib'
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary'
 import { useProgressBar } from '@/shared/lib'
+import { ToastWrapper } from '@/shared/ui'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 import '@/app/styles/globals.scss'
@@ -13,7 +12,6 @@ import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
-import 'react-toastify/dist/ReactToastify.css'
 
 type AppPropsWithLayout = {
   Component: NextPageWithLayout
@@ -26,11 +24,11 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <StoreProvider>
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
-        <ErrorBoundary>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
-      </GoogleOAuthProvider>
-
-      <ToastContainer />
+      <ToastWrapper>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
+          <ErrorBoundary>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
+        </GoogleOAuthProvider>
+      </ToastWrapper>
     </StoreProvider>
   )
 }
