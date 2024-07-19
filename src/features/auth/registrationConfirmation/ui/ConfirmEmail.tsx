581ab3b5dbd375ca322d178647b3a378edde64a1
VladimirKivg <cherenuha1@gmail.com>
2024-07-19 16:54:39 -0700

    translate CofirmEmail and rename one varible in the header

--- src/features/auth/registrationConfirmation/ui/ConfirmEmail.tsx
+++ src/features/auth/registrationConfirmation/ui/ConfirmEmail.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 import SignInImg from '@/shared/assets/img/sign-up_bro.png'
 import { SIGN_IN } from '@/shared/config/router'
+import { translate } from '@/shared/lib/langSwitcher'
 import { Button } from '@/shared/ui'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import s from './Confirmation.module.scss'
 
 export const ConfirmEmail = () => {
+  const t = translate(useRouter().locale)
+
   return (
     <div className={s.root}>
       <div className={s.textWrapper}>
-        <h2 className={s.title}>Congratulations!</h2>
-        <div>Your email has been confirmed</div>
+        <h2 className={s.title}>{t['Congratulations!']}</h2>
+        <div>{t['Your email has been confirmed']}</div>
         <Button as={Link} href={SIGN_IN}>
           Sign In
         </Button>
