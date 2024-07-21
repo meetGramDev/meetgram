f55d3794904234e06caffa26e38b3062d908dc99
AlexandrIvanov <alexandr.1996@list.ru>
2024-07-22 06:58:52 +0900

    fix storybook

--- src/features/userSettings/ui/UserSettings.stories.tsx
+++ src/features/userSettings/ui/UserSettings.stories.tsx
@@ -5,7 +5,26 @@ import { StoreDecorator } from '@/shared/config/storybook'
 import { fn } from '@storybook/test'
 
 const meta = {
-  args: {},
+  args: {
+    data: {
+      aboutMe: '',
+      avatars: {
+        // @ts-ignore
+        createdAt: '',
+        fileSize: 25,
+        height: 25,
+        url: '',
+        width: 25,
+      },
+      city: '',
+      createdAt: '',
+      dateOfBirth: '',
+      firstName: 'John',
+      id: 1,
+      lastName: 'Doe',
+      userName: 'John Doe username',
+    },
+  },
   component: UserSettingsForm,
   decorators: [StoreDecorator()],
   parameters: {
