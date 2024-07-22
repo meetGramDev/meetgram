913df4836c01135fb0ad0fc566b66e9ad442b780
m1rade <forx98@gmail.com>
2024-07-22 12:47:43 +0300

    fix: merge conflict

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,33 +1,16 @@
-import { SelectCurrentUserName, User, useGetUserProfileQuery } from '@/entities/user'
-import { UploadPhoto, UploadedPhotoType } from '@/features/profile/uploadUserPhoto'
+import { SelectCurrentUserName, User } from '@/entities/user'
 import { useAppSelector } from '@/shared/config/storeHooks'
-import { useClientProgress } from '@/shared/lib'
 import { getAuthLayout } from '@/widgets/layouts'
 
 function Profile() {
-  const { data, isLoading } = useGetUserProfileQuery()
   const userName = useAppSelector(SelectCurrentUserName)
-
-  const profileAvatar: UploadedPhotoType | undefined =
-    data && data.avatars.length !== 0
-      ? {
-          height: data.avatars[0].height,
-          url: data.avatars[0].url,
-          width: data.avatars[0].width,
-        }
-      : undefined
-
-  useClientProgress(isLoading)
 
   const onClickHandler = () => {
     alert('Profile settings clicked')
   }
 
   return (
-    <div>
-      {userName && <User onProfileSettingsClicked={onClickHandler} userName={userName} />}
-      <UploadPhoto key={data?.avatars.length} profileAvatar={profileAvatar} />
-    </div>
+    <div>{userName && <User onProfileSettingsClicked={onClickHandler} userName={userName} />}</div>
   )
 }
 
