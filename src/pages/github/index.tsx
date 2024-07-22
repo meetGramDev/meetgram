5ae34b20f56cb0b9949118ef8fce4ce607000b74
m1rade <forx98@gmail.com>
2024-07-22 19:15:43 +0300

    chore: style a github page

--- src/pages/github/index.tsx
+++ src/pages/github/index.tsx
@@ -1,9 +1,11 @@
 import { useLoginGithub } from '@/features/auth/by-oauth'
+import { useClientProgress } from '@/shared/lib'
 
 const RedirectFromGithub = () => {
   useLoginGithub()
+  useClientProgress(true)
 
-  return <div></div>
+  return <p className={'pt-[50vh] text-center text-regular16'}>Wait a second...</p>
 }
 
 export default RedirectFromGithub
