ab15c0b3fb053774da9455f6a18f81cc8944867f
m1rade <forx98@gmail.com>
2024-07-23 14:57:51 +0300

    feat: add confirmation dialog for deleting photo

--- src/features/profile/uploadUserPhoto/ui/UploadPhoto.tsx
+++ src/features/profile/uploadUserPhoto/ui/UploadPhoto.tsx
@@ -11,6 +11,7 @@ import { Button, Dialog } from '@/shared/ui'
 import s from './UploadPhoto.module.scss'
 
 import { UploadedPhotoType } from '../types/types'
+import { DeletePhotoForm } from './DeletePhotoForm'
 import { UploadPhotoForm } from './UploadPhotoForm'
 
 type Props = {
@@ -22,12 +23,11 @@ export const UploadPhoto = ({ profileAvatar }: Props) => {
   const [remove, { isLoading: isRemoveLoading }] = useDeleteProfilePhotoMutation()
 
   const [avatar, setAvatar] = useState<UploadedPhotoType | undefined>(profileAvatar)
-  const [open, setOpen] = useState(false)
+  const [openUpload, setOpenUpload] = useState(false)
+  const [openDelete, setOpenDelete] = useState(false)
   const [error, setError] = useState('')
 
   useClientProgress(isUploadLoading || isRemoveLoading)
-
-  const handleDialogClose = (open: boolean) => setOpen(open)
 
   const handleUploadPhoto = async (file: File) => {
     const formData = new FormData()
@@ -40,7 +40,7 @@ export const UploadPhoto = ({ profileAvatar }: Props) => {
       const avatar = resp.avatars[0]
 
       setAvatar({ height: avatar.height, url: avatar.url, width: avatar.width })
-      setOpen(false)
+      setOpenUpload(false)
     } catch (error) {
       console.error(error)
 
@@ -48,13 +48,21 @@ export const UploadPhoto = ({ profileAvatar }: Props) => {
     }
   }
 
-  const handleDeletePhoto = async () => {
+  const handleDeletePhoto = async (confirm: boolean) => {
+    if (!confirm) {
+      setOpenDelete(false)
+
+      return
+    }
+
     try {
       await remove().unwrap()
 
       setAvatar(undefined)
     } catch (error) {
       console.error(error)
+    } finally {
+      setOpenDelete(false)
     }
   }
 
@@ -63,20 +71,29 @@ export const UploadPhoto = ({ profileAvatar }: Props) => {
       {!avatar ? (
         <Photo type={'empty'} />
       ) : (
-        <Photo
-          alt={'Avatar'}
-          height={avatar?.height}
-          onDelete={handleDeletePhoto}
-          priority
-          src={avatar.url}
-          width={avatar?.width}
-        />
+        <>
+          <Photo
+            alt={'Avatar'}
+            height={avatar?.height}
+            onDelete={() => setOpenDelete(true)}
+            priority
+            src={avatar.url}
+            width={avatar?.width}
+          />
+          <Dialog
+            onOpenChange={() => setOpenDelete(false)}
+            open={openDelete}
+            title={'Delete photo'}
+          >
+            <DeletePhotoForm onConfirm={confirm => handleDeletePhoto(confirm)} />
+          </Dialog>
+        </>
       )}
 
       <Dialog
         className={s.dialog}
-        onOpenChange={handleDialogClose}
-        open={open}
+        onOpenChange={open => setOpenUpload(open)}
+        open={openUpload}
         title={'Add a Profile Photo'}
         trigger={
           <Button className={s.button} fullWidth variant={'outlined'}>
