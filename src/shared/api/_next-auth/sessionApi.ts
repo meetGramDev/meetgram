014dea45e313a4a7b0065bb52ec8a7976148f56f
m1rade <forx98@gmail.com>
2024-07-25 14:31:17 +0300

    fix: change env name

--- src/shared/api/_next-auth/sessionApi.ts
+++ src/shared/api/_next-auth/sessionApi.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const nextSessionInstance = axios.create({
-  baseURL: process.env.NEXT_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 })
 
