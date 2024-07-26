91bea9a01fc99c2b9e25f194c5b694b23f7574e6
AlexandrIvanov <alexandr.1996@list.ru>
2024-07-27 00:07:57 +0300

    fix styles, remove unused components, refactoring

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,21 +1,11 @@
 import { User, selectCurrentUserName } from '@/entities/user'
 import { useAppSelector } from '@/shared/config/storeHooks'
-import { getMainLayout } from '@/widgets/layouts/ui/MainLayout'
-
-import s from './index.module.scss'
+import { getMainLayout } from '@/widgets/layouts/ui/MainLayout/MainLayout'
 
 function Profile() {
   const userName = useAppSelector(selectCurrentUserName)
 
-  const onClickHandler = () => {
-    alert('Profile settings clicked')
-  }
-
-  return (
-    <div className={s.root}>
-      {userName && <User onProfileSettingsClicked={onClickHandler} userName={userName} />}
-    </div>
-  )
+  return <User userName={userName} />
 }
 
 Profile.getLayout = getMainLayout
--- src/widgets/layouts/ui/AuthLayout.tsx
+++ src/widgets/layouts/ui/AuthLayout.tsx
@@ -1,8 +1,6 @@
 import type { ReactElement, ReactNode } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
-import { selectIsUserAuth } from '@/entities/user'
-import { useAppSelector } from '@/shared/config/storeHooks'
 import { Header } from '@/widgets/header'
 import { MobileHeader } from '@/widgets/mobileHeader'
 import { Inter } from 'next/font/google'
@@ -11,11 +9,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   const isMobile = useMediaQuery({ query: '(max-width: 650px)' })
-  const isAuth = useAppSelector(selectIsUserAuth)
 
   return (
     <div className={`min-h-[calc(100vh_-_3.75rem_-_72px)] space-y-9 pb-9 ${inter.className}`}>
-      {isMobile ? <MobileHeader isAuth={isAuth} /> : <Header isAuth={isAuth} />}
+      {isMobile ? <MobileHeader /> : <Header />}
       <main className={'flex h-full items-start justify-center'}>{children}</main>
     </div>
   )
