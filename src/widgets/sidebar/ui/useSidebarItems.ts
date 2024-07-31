8bd843484e3e1a034cf5caed3a5d4cb1be3de806
mrAxekash <aliaxandr_ku@mail.ru>
2024-07-31 23:03:01 +0300

    fix: change pathname at Home in sidebar hook

--- src/widgets/sidebar/ui/useSidebarItems.ts
+++ src/widgets/sidebar/ui/useSidebarItems.ts
@@ -8,16 +8,18 @@ import { MyProfileIcon } from '@/shared/assets/icons/MyProfile'
 import { Search } from '@/shared/assets/icons/Search'
 import { StatisticsIcon } from '@/shared/assets/icons/Statistics'
 import { HOME } from '@/shared/config/router'
+import { useAppSelector } from '@/shared/config/storeHooks'
 import { translate } from '@/shared/lib/langSwitcher'
 import { useRouter } from 'next/router'
 
 export function useGetSidebarItems() {
   const { locale } = useRouter()
   const sidebarTr = translate(locale).sidebarTr
+  const userId = useAppSelector(state => state.user.accountData.userId)
 
   return useMemo(() => {
     return [
-      { name: sidebarTr.home, path: HOME, svg: HomeIcon },
+      { name: sidebarTr.home, path: `${HOME}/${userId}`, svg: HomeIcon },
       { name: sidebarTr.create, path: '', svg: CreateIcon },
       { name: sidebarTr.myProfile, path: '', svg: MyProfileIcon },
       { name: sidebarTr.messenger, path: '', svg: MessengerIcon },
