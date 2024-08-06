0cd391cc797a3fb092a30249534a76b010256b52
VladimirKivg <cherenuha1@gmail.com>
2024-08-06 09:08:42 -0700

    added useTranslate hook

--- src/widgets/langSwitcher/i18n/LangSwitcher.tsx
+++ src/widgets/langSwitcher/i18n/LangSwitcher.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { useMediaQuery } from 'react-responsive'
 
-import { ByFlagIcon } from '@/shared/assets/icons/BY'
-import { EsFlagIcon } from '@/shared/assets/icons/ES'
-import { RuFlagIcon } from '@/shared/assets/icons/RU'
-import { UaFlagIcon } from '@/shared/assets/icons/UA'
-import { UsFlagIcon } from '@/shared/assets/icons/US'
+import { translate } from '@/shared/lib/langSwitcher'
 import { Option } from '@/shared/ui/select/option'
 import { Select } from '@/shared/ui/select/select'
 import { useRouter } from 'next/router'
@@ -20,29 +16,11 @@ export const LangSwitcher = ({ className }: Props) => {
   const router = useRouter()
   const { asPath, locale, locales, pathname, push, query } = router
 
-  type TypeLanguage = {
-    be: {}
-    en: {}
-    es: {}
-    ru: {}
-    uk: {}
-  }
-  const languages: TypeLanguage = {
-    be: { componentLang: ByFlagIcon, language: 'Belarus' },
-    en: { componentLang: UsFlagIcon, language: 'English' },
-    es: { componentLang: EsFlagIcon, language: 'Spanish' },
-    ru: { componentLang: RuFlagIcon, language: 'Russia' },
-    uk: { componentLang: UaFlagIcon, language: 'Ukraine' },
-  }
-
   function changeLanguage(e: string) {
     push({ pathname, query }, asPath, { locale: e })
   }
 
-  const loc = locale || 'el'
-
-  // @ts-ignore
-  const Lang = languages[locale].componentLang
+  const Lang = translate(locale).componentLang
 
   const isMobile = useMediaQuery({ query: '(max-width:650px)' })
 
@@ -52,18 +30,18 @@ export const LangSwitcher = ({ className }: Props) => {
         onValueChange={changeLanguage}
         placeholder={
           <div className={s.row}>
-            <Lang />
+            <Lang /> {!isMobile && translate(locale).language}
           </div>
         }
       >
         {locales?.map((el, i) => {
-          // @ts-ignore
-          const Lang = languages[el].componentLang
+          const Lang = translate(el).componentLang
 
           return (
             <Option key={i} value={el}>
               <div className={s.row}>
                 <Lang />
+                {!isMobile && translate(el).language}
               </div>
             </Option>
           )
