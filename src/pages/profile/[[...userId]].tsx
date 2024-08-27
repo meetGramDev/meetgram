2bd4fd2c629442908b088b799643272385efb982
m1rade <forx98@gmail.com>
2024-08-27 16:59:35 +0300

    refactor: add suspense and replace posts query

--- src/pages/profile/[[...userId]].tsx
+++ src/pages/profile/[[...userId]].tsx
@@ -1,10 +1,8 @@
-import { useCallback, useState } from 'react'
+import { Suspense } from 'react'
 
-import { useGetPublicPostsQuery } from '@/entities/post'
 import { User, selectCurrentUserName, useFullUserProfileQuery } from '@/entities/user'
 import { UserSkeleton } from '@/entities/user/ui/skeletons/UserSkeleton'
 import { useAppSelector } from '@/shared/config/storeHooks'
-import { useClientProgress } from '@/shared/lib'
 import { NextPageWithLayout } from '@/shared/types'
 import { Loader } from '@/shared/ui'
 import { getMainLayout } from '@/widgets/layouts/ui/MainLayout/MainLayout'
@@ -12,56 +10,30 @@ import { PostsList } from '@/widgets/postsList'
 import { skipToken } from '@reduxjs/toolkit/query'
 import { useRouter } from 'next/router'
 
-const PAGE_SIZE = 12
-
 const UserId: NextPageWithLayout = () => {
   const router = useRouter()
   const userName = useAppSelector(selectCurrentUserName)
-  const { data: userData, isLoading: userProfileLoading } = useFullUserProfileQuery(
-    userName || skipToken
-  )
+  const {
+    data: userData,
+    isError: userProfileQueryError,
+    isLoading: userProfileLoading,
+  } = useFullUserProfileQuery(userName || skipToken)
 
   const userId = router.query.userId
-
-  // =========== //
-  const [endCursorPostId, setEndCursorPostId] = useState<number | undefined>(undefined)
-
-  const {
-    data: publicPosts,
-    isFetching: publicPostsFetching,
-    isLoading: publicPostsLoading,
-  } = useGetPublicPostsQuery({
-    endCursorPostId,
-    id: String(userData?.id),
-    params: { pageSize: PAGE_SIZE },
-  })
-
-  useClientProgress(publicPostsFetching)
-
-  const handleFetchNextPosts = useCallback(() => {
-    if (publicPosts?.items && publicPosts.items.length >= PAGE_SIZE) {
-      setEndCursorPostId(publicPosts?.items.at(-1)?.id)
-    }
-  }, [publicPosts?.items])
 
   return (
     <div className={'h-full'}>
       {!userProfileLoading && userData ? <User userData={userData} /> : <UserSkeleton />}
 
-      {!publicPostsLoading && publicPosts && publicPosts.items.length !== 0 ? (
-        <>
-          <PostsList fetchNextPosts={handleFetchNextPosts} posts={publicPosts?.items} />
-          {publicPostsFetching && (
-            <div className={'flex justify-center'}>
-              <Loader />
-            </div>
-          )}
-        </>
-      ) : (
-        <div className={'flex justify-center'}>
-          <Loader />
-        </div>
-      )}
+      <Suspense
+        fallback={
+          <div className={'flex justify-center'}>
+            <Loader />
+          </div>
+        }
+      >
+        <PostsList skipFetchingPosts={userProfileQueryError} userId={userData?.id} />
+      </Suspense>
     </div>
   )
 }
--- src/widgets/postsList/ui/PostsList.tsx
+++ src/widgets/postsList/ui/PostsList.tsx
@@ -1,26 +1,62 @@
+import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
-import { PublicPost } from '@/entities/post'
+import { useGetPublicPostsQuery } from '@/entities/post'
 import { useInfiniteScroll } from '@/shared/lib'
+import { Loader } from '@/shared/ui'
 
 import { PostsListDesktop } from './postsListDesktop/PostsListDesktop'
 import { PostsListMobile } from './postsListMobile/PostsListMobile'
 
 type Props = {
-  fetchNextPosts: () => void
-  posts: PublicPost[]
+  skipFetchingPosts: boolean
+  userId: number | undefined
 }
 
-export const PostsList = ({ fetchNextPosts, posts }: Props) => {
+const PAGE_SIZE = 12
+
+export const PostsList = ({ skipFetchingPosts, userId }: Props) => {
   const isMobile = useMediaQuery({ query: '(max-width: 650px)' })
 
-  const { ref, scroll } = useInfiniteScroll(fetchNextPosts)
+  // =========== //
+  const [endCursorPostId, setEndCursorPostId] = useState<number | undefined>(undefined)
+
+  const { ref, scroll } = useInfiniteScroll(() => {
+    if (publicPosts?.items && publicPosts.items.length >= PAGE_SIZE) {
+      setEndCursorPostId(publicPosts?.items.at(-1)?.id)
+    }
+  })
+
+  const {
+    data: publicPosts,
+    isFetching: publicPostsFetching,
+    isLoading: publicPostsLoading,
+  } = useGetPublicPostsQuery(
+    {
+      endCursorPostId,
+      id: String(userId),
+      params: { pageSize: PAGE_SIZE },
+    },
+    { skip: (!userId && !endCursorPostId) || skipFetchingPosts }
+  )
 
   return (
     <>
-      {isMobile ? <PostsListMobile posts={posts} /> : <PostsListDesktop posts={posts} />}
+      {isMobile ? (
+        <PostsListMobile posts={publicPosts?.items || []} />
+      ) : (
+        <PostsListDesktop posts={publicPosts?.items || []} />
+      )}
 
-      {scroll > 0 && <div className={'invisible h-4 w-full'} ref={ref}></div>}
+      {!publicPostsLoading && scroll > 0 && (
+        <div className={'invisible h-4 w-full'} ref={ref}></div>
+      )}
+
+      {publicPostsFetching && (
+        <div className={'flex justify-center'}>
+          <Loader />
+        </div>
+      )}
     </>
   )
 }
