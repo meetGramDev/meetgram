2672b22bd7d821195e1f0e81471900ab7cdc006b
mrAxekash <aliaxandr_ku@mail.ru>
2024-09-09 23:42:26 +0300

    feat: add scropping component

--- src/features/profile/addPost/ui/addScropping/AddScropping.tsx
+++ src/features/profile/addPost/ui/addScropping/AddScropping.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+import Cropper from 'react-easy-crop'
+
+import ArrowBack from '@/shared/assets/icons/arrow-back.svg'
+import { Button } from '@/shared/ui'
+import { ButtonIcon } from '@/shared/ui/buttonIcon/ButtonIcon'
+import Image from 'next/image'
+
 import s from './AddScropping.module.scss'
 
 export const AddScropping = () => {
-  return <div className={s.wrapper}>Scropping</div>
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  const handlePrevView = () => {}
+  const onNextState = () => {}
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.header}>
+        <ButtonIcon onClick={handlePrevView}>
+          <Image alt={'arrow-back'} src={ArrowBack} />
+        </ButtonIcon>
+        <div>Cropping</div>
+        <Button onClick={onNextState} variant={'text'}>
+          Next
+        </Button>
+      </div>
+      <div>{/*{<Cropper crop={crop} image={''} onCropChange={setCrop} zoom={zoom} />}*/}</div>
+    </div>
+  )
 }
