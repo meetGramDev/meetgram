8cea76cb275eb5c65a9e9ebe03b07eba9514b6d7
mrAxekash <aliaxandr_ku@mail.ru>
2024-09-14 23:25:46 +0300

    feat: add new button at select

--- src/features/profile/addPost/ui/addPostSettingsSelect/AddPostSettingsSelect.tsx
+++ src/features/profile/addPost/ui/addPostSettingsSelect/AddPostSettingsSelect.tsx
@@ -1,5 +1,8 @@
 import { Expand } from '@/shared/assets/icons/Expand'
+import { HorizontalRectangle } from '@/shared/assets/icons/HorizontalRectangle'
 import { ImageIcon } from '@/shared/assets/icons/ImageIcon'
+import { Rectangle } from '@/shared/assets/icons/Rectangle'
+import { Rectangular } from '@/shared/assets/icons/Rectangular'
 import { Button, Select } from '@/shared/ui'
 
 import s from './AddPostSettingsSelect.module.scss'
@@ -10,9 +13,23 @@ type Props = {
 
 export const AddPostSettingsSelect = ({ placeholder }: Props) => {
   return (
-    <Select contentClassName={s.selectContent} placeholder={placeholder} showArrow={false}>
+    <Select
+      contentClassName={s.selectContent}
+      placeholder={placeholder}
+      rootClassName={s.selectTrigger}
+      showArrow={false}
+    >
       <Button className={s.button} onClick={() => {}} variant={'text'}>
         Оригинал <ImageIcon />{' '}
+      </Button>
+      <Button className={s.button} onClick={() => {}} variant={'text'}>
+        1:1 <Rectangle />{' '}
+      </Button>
+      <Button className={s.button} onClick={() => {}} variant={'text'}>
+        4:5 <Rectangular />{' '}
+      </Button>
+      <Button className={s.button} onClick={() => {}} variant={'text'}>
+        16:9 <HorizontalRectangle />{' '}
       </Button>
     </Select>
   )
