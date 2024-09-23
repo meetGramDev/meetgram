bdba34dc6623c5d9a0868a78cd7853193ce555b4
m1rade <forx98@gmail.com>
2024-09-24 00:56:40 +0300

    fix: styles

--- src/shared/ui/imageCarousel/ImageCarousel.tsx
+++ src/shared/ui/imageCarousel/ImageCarousel.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
   Carousel,
@@ -38,92 +38,88 @@ type Props = {
   showNavigation?: boolean
 } & CarouselProps
 
-export const ImageCarousel = memo(
-  ({
-    className,
-    contentClassname,
-    images,
-    itemClassname,
-    options,
-    showNavigation = true,
-    ...props
-  }: Props) => {
-    const [api, setApi] = useState<CarouselApi>()
+export const ImageCarousel = ({
+  className,
+  contentClassname,
+  images,
+  itemClassname,
+  options,
+  showNavigation = true,
+  ...props
+}: Props) => {
+  const [api, setApi] = useState<CarouselApi>()
 
-    useEffect(() => {
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    const preventEdgeScrolling = function (api: CarouselApi) {
       if (!api) {
         return
       }
 
-      const preventEdgeScrolling = function (api: CarouselApi) {
-        if (!api) {
-          return
-        }
+      const { limit, location, offsetLocation, scrollBody, scrollTo, target, translate } =
+        api.internalEngine()
 
-        const { limit, location, offsetLocation, scrollBody, scrollTo, target, translate } =
-          api.internalEngine()
+      let edge: null | number = null
 
-        let edge: null | number = null
-
-        if (limit.reachedMax(target.get())) {
-          edge = limit.max
-        }
-        if (limit.reachedMin(target.get())) {
-          edge = limit.min
-        }
-
-        if (edge !== null) {
-          offsetLocation.set(edge)
-          location.set(edge)
-          target.set(edge)
-          translate.to(edge)
-          translate.toggleActive(false)
-          scrollBody.useDuration(0).useFriction(0)
-          scrollTo.distance(0, false)
-        } else {
-          translate.toggleActive(true)
-        }
+      if (limit.reachedMax(target.get())) {
+        edge = limit.max
+      }
+      if (limit.reachedMin(target.get())) {
+        edge = limit.min
       }
 
-      api.on('scroll', preventEdgeScrolling)
-    }, [api])
+      if (edge !== null) {
+        offsetLocation.set(edge)
+        location.set(edge)
+        target.set(edge)
+        translate.to(edge)
+        translate.toggleActive(false)
+        scrollBody.useDuration(0).useFriction(0)
+        scrollTo.distance(0, false)
+      } else {
+        translate.toggleActive(true)
+      }
+    }
 
-    return (
-      <Carousel
-        className={clsx(s.carousel, className)}
-        dotsClassname={clsx(s.dots, images.length === 1 && s.invisible)}
-        options={{
-          ...options,
-          containScroll: 'trimSnaps',
-          skipSnaps: false,
-          watchDrag: images.length > 1,
-        }}
-        setApi={setApi}
-        {...props}
-      >
-        <CarouselContent className={clsx(contentClassname)}>
-          {images?.map((image, i) => (
-            <CarouselItem className={clsx(itemClassname, s.item)} key={i}>
-              <Image
-                alt={`Image-${i + 1}`}
-                className={s.photo}
-                {...('image' in image
-                  ? { height: 300, src: image.image, width: 300 }
-                  : { height: image.height, src: image.url, width: image.width })}
-              />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
+    api.on('scroll', preventEdgeScrolling)
+  }, [api])
 
-        {showNavigation && images.length > 1 && (
-          <>
-            <CarouselPrevious className={s.navigationPrev} />
-            <CarouselNext className={s.navigationNext} />
-          </>
-        )}
-      </Carousel>
-    )
-  }
-)
+  return (
+    <Carousel
+      className={clsx(s.carousel, className)}
+      dotsClassname={clsx(s.dots, images.length === 1 && s.invisible)}
+      options={{
+        ...options,
+        containScroll: 'trimSnaps',
+        skipSnaps: false,
+        watchDrag: images.length > 1,
+      }}
+      setApi={setApi}
+      {...props}
+    >
+      <CarouselContent className={clsx(contentClassname)}>
+        {images?.map((image, i) => (
+          <CarouselItem className={clsx(itemClassname, s.item)} key={i}>
+            <Image
+              alt={`Image-${i + 1}`}
+              className={s.photo}
+              {...('image' in image
+                ? { height: 300, src: image.image, width: 300 }
+                : { height: image.height, src: image.url, width: image.width })}
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
 
-ImageCarousel.displayName = 'ImageCarousel'
+      {showNavigation && images.length > 1 && (
+        <>
+          <CarouselPrevious className={s.navigationPrev} />
+          <CarouselNext className={s.navigationNext} />
+        </>
+      )}
+    </Carousel>
+  )
+}
