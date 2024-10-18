47017a68c6a1c1b798ab1169b24fba5b7c23c760
m1rade <forx98@gmail.com>
2024-10-19 02:37:36 +0300

    fix: small fixes

--- src/widgets/postsList/ui/PostsList.tsx
+++ src/widgets/postsList/ui/PostsList.tsx
@@ -33,6 +33,7 @@ export const PostsList = ({ isFollowing, isPublic, post, posts, userId }: PostsL
   const postsScrollRef = useRef<HTMLElement>(null)
   const { ref } = useInfiniteScroll(
     () => {
+      // TODO TODO TODO скролл с SSR постами
       if (isPublic && !firstRenderSkipPagination.current && posts && posts?.length >= PAGE_SIZE) {
         console.log(endCursorPostId)
         getPosts({
