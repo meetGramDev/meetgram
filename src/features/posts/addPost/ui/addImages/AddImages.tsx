50ed22269d4312268bd498510872ef590458473e
VladimirKivg <cherenuha1@gmail.com>
2024-10-25 00:42:35 -0700

    fix

--- src/features/posts/addPost/ui/addImages/AddImages.tsx
+++ src/features/posts/addPost/ui/addImages/AddImages.tsx
@@ -109,7 +109,7 @@ export const AddImages = () => {
         onClick={handleSelectFileClick}
         variant={'primary'}
       >
-        {t('Select from computer') as string}
+        {t('S' + 'elect from computer')}
       </Button>
     </div>
   )
