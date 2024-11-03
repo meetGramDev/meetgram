93a00f7b80133acdf3da3d25723cd705247d4788
m1rade <forx98@gmail.com>
2024-11-03 18:27:23 +0300

    add translation for PublicUserMetadata

--- src/entities/user/ui/userMetadata/PublicUserMetadata.tsx
+++ src/entities/user/ui/userMetadata/PublicUserMetadata.tsx
@@ -1,3 +1,5 @@
+import { useTranslate } from '@/shared/lib/useTranslate'
+
 type Props = {
   followersCount: number
   followingCount: number
@@ -9,22 +11,24 @@ export const PublicUserMetadata = ({
   followingCount,
   publicationsCount,
 }: Props) => {
+  const t = useTranslate()
+
   return (
     <div className={'mb-6 flex items-center justify-start gap-24'}>
       <div className={'text-regular14'}>
         <span className={'font-bold'}>{followingCount}</span>
         <br />
-        Following
+        {t('Following')}
       </div>
       <div className={'text-regular14'}>
         <span className={'font-bold'}>{followersCount}</span>
         <br />
-        Followers
+        {t('Followers')}
       </div>
       <div className={'text-regular14'}>
         <span className={'font-bold'}>{publicationsCount}</span>
         <br />
-        Publications
+        {t('Publications')}
       </div>
     </div>
   )
