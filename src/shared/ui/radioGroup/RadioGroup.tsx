229a2b75e28039900feb4cd013a5b3686aabb2ad
VyacheslavSavchik <rothmans.830@gmail.com>
2024-11-09 19:04:39 +0300

    add: Radio Group

---
+++ src/shared/ui/radioGroup/RadioGroup.tsx
@@ -0,0 +1,34 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import * as RadixRadioGroup from '@radix-ui/react-radio-group'
+import { clsx } from 'clsx'
+
+import s from './RadioGroup.module.scss'
+
+import { RadioItem } from './radioItem/RadioGroupItem'
+
+export type RadioGroupProps = {
+  options: {
+    checked?: boolean
+    disabled?: boolean
+    label: string
+    value: string
+  }[]
+} & Omit<ComponentPropsWithoutRef<typeof RadixRadioGroup.Root>, 'asChild'>
+
+export const RadioGroup = forwardRef<ElementRef<typeof RadixRadioGroup.Root>, RadioGroupProps>(
+  (props, ref) => {
+    const { className, defaultChecked, options, ...rest } = props
+
+    return (
+      <RadixRadioGroup.Root className={clsx(s.radioGroup, className)} {...rest} ref={ref}>
+        {options.map((el, index) => (
+          <span className={clsx(s.labelWrapper, el.disabled && s.disabled, className)} key={index}>
+            <RadioItem {...el} />
+            <label>{el.label}</label>
+          </span>
+        ))}
+      </RadixRadioGroup.Root>
+    )
+  }
+)
