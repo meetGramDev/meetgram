d1965684768b05785fa145c6b731d547a648e72c
mrAxekash <aliaxandr_ku@mail.ru>
2024-12-12 22:39:40 +0300

    fix: fix button clickHandler for payment after review teamlead and add new changes after merge project

--- src/features/profile/userManagement/ui/UserManagement.tsx
+++ src/features/profile/userManagement/ui/UserManagement.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { MouseEventHandler, useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 
 import { ServerMessagesType } from '@/shared/api'
@@ -139,6 +139,18 @@ export const UserManagement = () => {
 
   useClientProgress(isLoading || cancelAutoRenewalLoading)
 
+  const onPaymentClickHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    paymentMethod: PaymentType
+  ) => {
+    e.preventDefault()
+    if (costOfPaymentData !== undefined) {
+      const requestData = dataPacking(paymentMethod)
+
+      handleSubmitForm(requestData)
+    }
+  }
+
   return (
     <>
       {data && costOfPaymentData ? (
@@ -196,15 +208,9 @@ export const UserManagement = () => {
               <div className={s.paymentWrapper}>
                 <div className={s.paymentButtonWrapper}>
                   <Button
+                    as={'button'}
                     className={s.paymentButton}
-                    onClick={e => {
-                      e.preventDefault()
-                      if (costOfPaymentData !== undefined) {
-                        const requestData = dataPacking('PAYPAL')
-
-                        handleSubmitForm(requestData)
-                      }
-                    }}
+                    onClick={e => onPaymentClickHandler(e, 'PAYPAL')}
                     type={'submit'}
                     variant={'outlined'}
                   >
@@ -213,14 +219,7 @@ export const UserManagement = () => {
                   or
                   <Button
                     className={s.paymentButton}
-                    onClick={e => {
-                      e.preventDefault()
-                      if (costOfPaymentData !== undefined) {
-                        const requestData = dataPacking('STRIPE')
-
-                        handleSubmitForm(requestData)
-                      }
-                    }}
+                    onClick={e => onPaymentClickHandler(e, 'PAYPAL')}
                     type={'submit'}
                     variant={'outlined'}
                   >
