d7be0ec4977fe89331999d4437b60796a56fcf2c
m1rade <forx98@gmail.com>
2025-05-11 15:30:33 +0300

    feat: adjust a textarea height to the typed text height

--- src/features/messenger/ui/MessageInput.tsx
+++ src/features/messenger/ui/MessageInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent, useState } from 'react'
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
 
 import { TextArea } from '@/shared/ui'
 
@@ -10,10 +10,21 @@ type Props = {
 
 export const MessageInput = ({ onMessage }: Props) => {
   const [message, setMessage] = useState('')
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
 
   const registerMessage = (value?: string) => {
     onMessage?.(value ?? message)
     setMessage('')
+  }
+
+  // Adjust height based on content
+  const adjustHeight = () => {
+    if (textareaRef.current) {
+      // Reset height to auto to get the correct scrollHeight
+      textareaRef.current.style.height = 'auto'
+      // Set the height to match the content (scrollHeight)
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+    }
   }
 
   const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -38,15 +49,21 @@ export const MessageInput = ({ onMessage }: Props) => {
     }
   }
 
+  useEffect(() => {
+    adjustHeight()
+  }, [message])
+
   return (
     <>
       <TextArea
         className={
-          'border-x-0 border-b-0 border-dark-300 bg-dark-900 pr-[114px] placeholder:text-light-900'
+          'max-h-20 min-h-12 overflow-auto border-x-0 border-b-0 border-dark-300 bg-dark-900 pr-[114px] placeholder:text-light-900'
         }
         onChange={handleMessageChange}
         onKeyDown={handleKeyDown}
         placeholder={'Type message... (shift+enter to send)'}
+        ref={textareaRef}
+        rows={1}
         value={message}
       />
       <div className={'absolute right-5 top-1/2 z-10 -translate-y-1/2'}>
